c8c1d92e5baf8f5384f8aeab2be30f99b95fe502
GabrielManzanilla <gabriel29444@gmail.com>
2025-03-19 15:51:49 -0600

    Feat: Correcion en importacion de SubmitForm en main.js

--- public/js/register/main.js
+++ public/js/register/main.js
@@ -1,82 +1,104 @@
-import { FormNavigator } from './formNavigation.js';
-import { CurpService } from './curpService.js';
-import { PostalCodeService } from './postalCodeService.js';
-import { TableManager } from './tableManager.js';
-import { FillInputs } from './fillInputs.js';
-import { SubmitForm } from './submit.js';
+import { FormNavigator } from "./formNavigation.js";
+import { CurpService } from "./curpService.js";
+import { PostalCodeService } from "./postalCodeService.js";
+import { TableManager } from "./tableManager.js";
+import { FillInputs } from "./fillInputs.js";
+import { SubmitForm } from "./submit.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formNavigator = new FormNavigator(document.querySelector('form'), '#progressBar');
-  const curpService = new CurpService('pruebas');
-  const postalService = new PostalCodeService('pruebas');
-	const fillInputs = new FillInputs();
+document.addEventListener("DOMContentLoaded", () => {
+  const formNavigator = new FormNavigator(
+    document.querySelector("form"),
+    "#progressBar"
+  );
+  const curpService = new CurpService("pruebas");
+  const postalService = new PostalCodeService("pruebas");
+  const fillInputs = new FillInputs();
   const submitForm = new SubmitForm();
-  
-  // Eventos
-  document.getElementById('search_curp_btn').addEventListener('click', async () => {
-    const curpInput = document.getElementById('curp_usuario');
-    if (CurpService.validarCurpFormato(curpInput.value)) {
-      const data = await curpService.consultarCurp(curpInput.value);
-      fillInputs.fillInputsCurp(data);
-			curpInput.classList.add('is-valid');	
-    } else {
-      curpInput.classList.add('is-invalid');
-    }
 
-  });
-  
-  document.getElementById('search_cp_btn').addEventListener('click', async () => {
-    const cp = document.getElementById('codigo_postal').value;
-    const data_location = await postalService.consultarCodigoPostal(cp);
-    fillInputs.fillInputsCodePostal(data_location);
-  });
-  
-  document.getElementById('add_address').addEventListener('click', () => {
-    const addressManager = new TableManager('#address_table');
+  // Eventos
+  document
+    .getElementById("search_curp_btn")
+    .addEventListener("click", async () => {
+      const curpInput = document.getElementById("curp_usuario");
+      if (CurpService.validarCurpFormato(curpInput.value)) {
+        const data = await curpService.consultarCurp(curpInput.value);
+        fillInputs.fillInputsCurp(data);
+        curpInput.classList.add("is-valid");
+      } else {
+        curpInput.classList.add("is-invalid");
+      }
+    });
+
+  document
+    .getElementById("search_cp_btn")
+    .addEventListener("click", async () => {
+      const cp = document.getElementById("codigo_postal").value;
+      const data_location = await postalService.consultarCodigoPostal(cp);
+      fillInputs.fillInputsCodePostal(data_location);
+    });
+
+  document.getElementById("add_address").addEventListener("click", () => {
+    const addressManager = new TableManager("#address_table");
     addressManager.checkEmpty(); // Mostrar mensaje vacío inicial
-    const direccion = document.getElementById('direccion_usuario').value;
-    const municipio = document.getElementById('municipio_usuario').value;
-    const localidad = document.getElementById('localidad_usuario').value;
-    const estado = document.getElementById('estado_usuario').value;
+    const direccion = document.getElementById("direccion_usuario").value;
+    const municipio = document.getElementById("municipio_usuario").value;
+    const localidad = document.getElementById("localidad_usuario").value;
+    const estado = document.getElementById("estado_usuario").value;
     addressManager.addRow([direccion, municipio, localidad, estado]);
   });
 
-  document.getElementById('add_registro_fiscal').addEventListener('click', () => {
-    const fiscalManager = new TableManager('#registros_fiscales_table');
-    fiscalManager.checkEmpty(); // Mostrar mensaje vacío inicial
-    const nombre_registro = document.getElementById('nombre_registro').value;
-    const clave_registro = document.getElementById('clave_registro').value;
-    fiscalManager.addRow([nombre_registro, clave_registro]);
-  });
+  document
+    .getElementById("add_registro_fiscal")
+    .addEventListener("click", () => {
+      const fiscalManager = new TableManager("#registros_fiscales_table");
+      fiscalManager.checkEmpty(); // Mostrar mensaje vacío inicial
+      const nombre_registro = document.getElementById("nombre_registro").value;
+      const clave_registro = document.getElementById("clave_registro").value;
+      fiscalManager.addRow([nombre_registro, clave_registro]);
+    });
 
-  document.getElementById('add_producto').addEventListener('click', () => {
-    const productManager = new TableManager('#productos_table');
+  document.getElementById("add_producto").addEventListener("click", () => {
+    const productManager = new TableManager("#productos_table");
     productManager.checkEmpty(); // Mostrar mensaje vacío inicial
-    const nombre_producto = document.getElementById('nombre_producto').value;
-    const produccion_mensual = document.getElementById('produccion_mensual').value;
-    const ventas_mensuales = document.getElementById('ventas_mensuales').value;
-    const ventas_anuales = document.getElementById('ventas_anuales').value;
-    productManager.addRow([nombre_producto, produccion_mensual, ventas_mensuales, ventas_anuales]);
+    const nombre_producto = document.getElementById("nombre_producto").value;
+    const produccion_mensual =
+      document.getElementById("produccion_mensual").value;
+    const ventas_mensuales = document.getElementById("ventas_mensuales").value;
+    const ventas_anuales = document.getElementById("ventas_anuales").value;
+    productManager.addRow([
+      nombre_producto,
+      produccion_mensual,
+      ventas_mensuales,
+      ventas_anuales,
+    ]);
   });
 
-  document.getElementById('add_medio_digital').addEventListener('click', () => {
-    const digitalManager = new TableManager('#medios_digitales_table');
+  document.getElementById("add_medio_digital").addEventListener("click", () => {
+    const digitalManager = new TableManager("#medios_digitales_table");
     digitalManager.checkEmpty(); // Mostrar mensaje vacío inicial
-    const nombre_medio = document.getElementById('nombre_medio_adicional').value;
-    const registro_medio = document.getElementById('medio_adicional_registro').value;
+    const nombre_medio = document.getElementById(
+      "nombre_medio_adicional"
+    ).value;
+    const registro_medio = document.getElementById(
+      "medio_adicional_registro"
+    ).value;
     digitalManager.addRow([nombre_medio, registro_medio]);
   });
 
-  document.getElementById('add_documento_adicional').addEventListener('click', () => {
-    const documentManager = new TableManager('#documentos_adicionales_table');
-    documentManager.checkEmpty(); // Mostrar mensaje vacío inicial
-    const denominacion_documento = document.getElementById('denominacion_documento_adicional').value;
-    const documento = document.getElementById('nombre_documento_adicional').files[0].name;
-    documentManager.addRow([denominacion_documento, documento]);
-  });
-  
+  document
+    .getElementById("add_documento_adicional")
+    .addEventListener("click", () => {
+      const documentManager = new TableManager("#documentos_adicionales_table");
+      documentManager.checkEmpty(); // Mostrar mensaje vacío inicial
+      const denominacion_documento = document.getElementById(
+        "denominacion_documento_adicional"
+      ).value;
+      const documento = document.getElementById("nombre_documento_adicional")
+        .files[0].name;
+      documentManager.addRow([denominacion_documento, documento]);
+    });
 
-  document.getElementById('submitForm').addEventListener('click', () => {
-    
-  });
+  // document.getElementById('submitForm').addEventListener('click', () => {
+
+  // });
 });
--- public/js/register/submit.js
+++ public/js/register/submit.js
@@ -1,4 +1,4 @@
-export class Submit {
+export class SubmitForm {
   constructor() {
     this.form = document.querySelector("form");
     this.form.addEventListener("submit", this.submit.bind(this));
@@ -6,113 +6,48 @@ export class Submit {
 
   submit(e) {
     //Datos de la primera seccion 'Datos Personales'
-    curp_usuario = document.getElementById("curp_usuario").value;
-    apellido_paterno = document.getElementById("apellido_paterno").value;
-    apellido_materno = document.getElementById("apellido_materno").value;
-    nombres_usuario = document.getElementById("nombres_usuario").value;
-    fecha_nacimiento = document.getElementById(
-      "fecha_nacimiento_usuario"
-    ).value;
-    entidad_nacimiento = document.getElementById(
-      "entidad_nacimiento_usuario"
-    ).value;
-    municipio_nacimiento = document.getElementById(
-      "municipio_nacimiento_usuario"
-    ).value;
-    sexo = document.getElementById("sexo").value;
+		const datos_personales =['curp_usuario','apellido_paterno','apellido_materno','nombres_usuario','fecha_nacimiento_usuario','entidad_nacimiento_usuario','municipio_nacimiento_usuario','sexo'];
+		const formDatas=this.getElementsValues(datos_personales);
+
     mayahablante = document.getElementById("mayahablante").checked;
 
     //Datos de la segunda seccion 'Domicilios'
     domicilios=this.getTableContent("address_table");
 
+
     //Datos de la tercera seccion 'Registros Fiscales'
-    actividad_economica = document.getElementById("actividad_economica").value;
-    razon_social = document.getElementById("razon_social_usuario").value;
-    nombre_comercial = document.getElementById("nombre_comercial").value;
-    tiempo_ejerciendo = document.getElementById("tiempo_ejerciendo").value;
-    establecimiento_usuario = document.getElementById(
-      "establecimiento_usuario"
-    ).value;
-    registro_affy_usuario = document.getElementById(
-      "registro_affy_usuario"
-    ).value;
-    registro_sat_usuario = document.getElementById(
-      "registro_sat_usuario"
-    ).value;
-    regimen_fiscal = document.getElementById("regimen_fiscal").value;
-    numero_empleados = document.getElementById("numero_empleados").value;
-    registro_impi_usuario = document.getElementById(
-      "registro_impi_usuario"
-    ).value;
-    registro_imss_usuario = document.getElementById(
-      "registro_imss_usuario"
-    ).value;
+		const registros_fiscales =['actividad_economica','razon_social_usuario','nombre_comercial','tiempo_ejerciendo','establecimiento_usuario','registro_affy_usuario','registro_sat_usuario','regimen_fiscal','numero_empleados','registro_impi_usuario','registro_imss_usuario'];
+		const formDatas2=this.getElementsValues(registros_fiscales);
+
 		registros_fiscales_adicionales=this.getTableContent("registros_fiscales_table");
 
 		//Datos de la cuarta seccion 'Productos'
 		productos= this.getTableContent("productos_table");
 		
 		//Datos de la quinta seccion 'Medios Digitales'
-		facebook_usuario = document.getElementById("facebook_usuario").value;
-		facebook_empresarial = document.getElementById("facebook_empresarial").value;
-		facebook_marketplace = document.getElementById("facebook_marketplace").value;
-		pagina_web = document.getElementById("pagina_web").value;
-		whatsapp_empresarial = document.getElementById("whatsapp_empresarial").value;
-		mercado_libre = document.getElementById("mercado_libre").value;
-		mercado_pago = document.getElementById("mercado_pago").value;
+		const medios_digitales=['facebook_usuario','facebook_empresarial','facebook_marketplace','pagina_web','whatsapp_empresarial','mercado_libre','mercado_pago'];
+		const formDatas3=this.getElementsValues(medios_digitales);
+		
 		medios_digitales_adicionales=this.getTableContent("medios_digitales_table");
 
 		//Datos de la sexta seccion 'Documentos'
-		ine_file = document.getElementById("ine_file").files[0];
-		acta_nacimiento_file = document.getElementById("acta_nacimiento_file").files[0];
-		comprobante_domicilio_file = document.getElementById("comprobante_domicilio_file").files[0];
-		cif_file = document.getElementById("cif_file").files[0];
-		affy_file = document.getElementById("affy_file").files[0];
-		impi_file = document.getElementById("impi_file").files[0];
-		imss_file = document.getElementById("imss_file").files[0];
+		const documentos=['ine_file','acta_nacimiento_file','comprobante_domicilio_file','cif_file','affy_file','impi_file','imss_file'];
+		const formDatas4=this.getFilesValues(documentos);
+
 		documentos_adicionales=this.getTableContent("documentos_adicionales_table");
 
 		//Se crea un objeto FormData para enviar los datos al servidor
 		const formData = new FormData();
-		formData.append("curp_usuario", curp_usuario);
-		formData.append("apellido_paterno", apellido_paterno);
-		formData.append("apellido_materno", apellido_materno);
-		formData.append("nombres_usuario", nombres_usuario);
-		formData.append("fecha_nacimiento", fecha_nacimiento);
-		formData.append("entidad_nacimiento", entidad_nacimiento);
-		formData.append("municipio_nacimiento", municipio_nacimiento);
-		formData.append("sexo", sexo);
-		formData.append("mayahablante", mayahablante);
-		formData.append("domicilios", JSON.stringify(domicilios));
-		formData.append("actividad_economica", actividad_economica);
-		formData.append("razon_social", razon_social);
-		formData.append("nombre_comercial", nombre_comercial);
-		formData.append("tiempo_ejerciendo", tiempo_ejerciendo);
-		formData.append("establecimiento_usuario", establecimiento_usuario);
-		formData.append("registro_affy_usuario", registro_affy_usuario);
-		formData.append("registro_sat_usuario", registro_sat_usuario);
-		formData.append("regimen_fiscal", regimen_fiscal);
-		formData.append("numero_empleados", numero_empleados);
-		formData.append("registro_impi_usuario", registro_impi_usuario);
-		formData.append("registro_imss_usuario", registro_imss_usuario);
-		formData.append("registros_fiscales_adicionales", JSON.stringify(registros_fiscales_adicionales));
-		formData.append("productos", JSON.stringify(productos));
-		formData.append("facebook_usuario", facebook_usuario);
-		formData.append("facebook_empresarial", facebook_empresarial);
-		formData.append("facebook_marketplace", facebook_marketplace);
-		formData.append("pagina_web", pagina_web);
-		formData.append("whatsapp_empresarial", whatsapp_empresarial);
-		formData.append("mercado_libre", mercado_libre);
-		formData.append("mercado_pago", mercado_pago);
-		formData.append("medios_digitales_adicionales", JSON.stringify(medios_digitales_adicionales));
-		formData.append("ine_file", ine_file);
-		formData.append("acta_nacimiento_file", acta_nacimiento_file);
-		formData.append("comprobante_domicilio_file", comprobante_domicilio_file);
-		formData.append("cif_file", cif_file);
-		formData.append("affy_file", affy_file);
-		formData.append("impi_file", impi_file);
-		formData.append("imss_file", imss_file);
-		formData.append("documentos_adicionales", JSON.stringify(documentos_adicionales));
+		formDatas.forEach((value,key)=>formData.append(key,value));
+		formData.append('mayahablante',mayahablante);
+		formDatas2.forEach((value,key)=>formData.append(key,value));
+		formData.append('domicilios',JSON.stringify(domicilios));
+		formDatas3.forEach((value,key)=>formData.append(key,value));
+		formData.append('registros_fiscales_adicionales',JSON.stringify(registros_fiscales_adicionales));
+		formData.append('productos',JSON.stringify(productos));
+		formData.append('medios_digitales_adicionales',JSON.stringify(medios_digitales_adicionales));
+		formData.append('documentos_adicionales',JSON.stringify(documentos_adicionales));
+		
   }
 
 
@@ -120,16 +55,46 @@ export class Submit {
     const array = [];
     const table = document.getElementById(table_id);
 
-    //este ciclo recorre las filas de la tabla de domicilios y agrega los datos a la lista, excepto la fila de mensaje
+    //este segmento comprueba que no se agregue los la fila de mensaje
     table.querySelectorAll("tr").forEach((tr) => {
-      if (row.id !== `fila_mensaje_${table_id}`) {
-        const direccion = tr.children[0].textContent;
-        const municipio = tr.children[1].textContent;
-        const localidad = tr.children[2].textContent;
-        const estado = tr.children[3].textContent;
-        array.push({ direccion, municipio, localidad, estado });
+			if (tr.id !== `fila_mensaje_${table_id}`) {
+
+				// este ciclo recorre las filas de la tabla de domicilios y agrega los datos a la lista a excepcion del boton para eliminar
+				const columns=Array.from(tr.children)
+														.slice(0,-1)
+														.map(td=>td.textContent.trim());
+        array.push(columns);
       }
     });
     return array;
   }
+
+	//elemenst es un array de ids de los elementos del formulario
+	getElementsValues(elements){
+		const formDatas = new FormData();
+		elements.forEach(element=>{
+			const input=document.getElementById(element);
+			if(input){
+				formDatas.append(element,input.value);
+			}
+			else{
+				console.warn(`Elemento ${element} no encontrado`);
+			}
+		});
+		return formDatas;
+	}
+
+	getFilesValues(elements){
+		const formDatas = new FormData();
+		elements.forEach(element=>{
+			const input=document.getElementById(element);
+			if(input){
+				formDatas.append(element,input.files[0]);
+			}
+			else{
+				console.warn(`Elemento ${element} no encontrado`);
+			}
+		});
+		return formDatas;
+	}
 }
