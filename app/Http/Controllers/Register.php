b4b9e0c4fadc4c49603549ea143687b08379c293
GabrielManzanilla <gabriel29444@gmail.com>
2025-04-20 16:21:34 -0600

    In Progress (Controllers/Register.php)
    The status is in progress to load data on the DB

--- app/Http/Controllers/Register.php
+++ app/Http/Controllers/Register.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Validator;
 
 //importacion de models
 use App\Models\Persona;
@@ -32,58 +33,64 @@ class Register extends Controller
     public function store(Request $request)
     {
         //
-        $validate = $request ->validate([
+
+        $validate = $request ->Validator::request([
             //DATOS PERSONALES
-            'nombres_usuario' => 'required | string | max:50',
-            'apellidos_paterno' => 'required | string | max:50',
-            'apellidos_materno' => 'required | string | max:50',
-            'curp_usuario'=> 'required | string | max:18',
-            'municipio_nacimiento' => 'required | string',
-            'estado_nacimiento' => 'required | string',
-            'fecha_nacimiento' => 'required | date',
-            'email_usuario' => 'required|email',
-            'telefono_usuario' => 'required | string | max:10', 
-            'estado_perfil_usuario' => 'required | string',
-            'estado_candidato' => 'required | string',
+            'nombres_usuario' => ' | string | max:50',
+            'apellidos_paterno' => ' | string | max:50',
+            'apellidos_materno' => ' | string | max:50',
+            'curp_usuario'=> ' | string | max:18',
+            'municipio_nacimiento' => ' | string',
+            'estado_nacimiento' => ' | string',
+            'fecha_nacimiento' => ' | date',
+            'email_usuario' => '|email',
+            'telefono_usuario' => ' | string | max:10', 
+            'estado_perfil_usuario' => ' | string',
+            'estado_candidato' => ' | string',
 
             //DATOS FISCALES
-            'actividad_economica' => 'required | string',
-            'regimen_fiscal' => 'required | string',
-            'nombre_comercial' => 'required | string | max:50',
-            'tiempo_ejerciendo' => 'required | date',
-            'registro_imss' => 'required | string | max:11',
-            'registro_affy' => 'required | string | max:11',
-            'rfc_usuario' => 'required | string | max:13',
-            'razon_social' => 'required | string | max:50',
+            'actividad_economica' => ' | string',
+            'regimen_fiscal' => ' | string',
+            'nombre_comercial' => ' | string | max:50',
+            'tiempo_ejerciendo' => ' | date',
+            'registro_imss' => ' | string | max:11',
+            'registro_affy' => ' | string | max:11',
+            'rfc_usuario' => ' | string | max:13',
+            'razon_social' => ' | string | max:50',
             'numero_empleados' => 'requiered| integer',
-            'registro_impi' => 'required | string | max:11',
+            'registro_impi' => ' | string | max:11',
             // -- falta la tabla de registros adicionales --
+            'registros_json' => '',
 
             //DOMICILIOS
-            // --falta la tabla de domicilios --
+            'domicilios_json' => '',
 
             //PRODUCTOS Y VENTAS
-            // --falta la tabla de productos y ventas --
+            'productos_json'=>'',
 
-            //MEDIOS ADICIONALES
-            'facebook_usuario' => 'required | string | max:50',
-            'facebook_empresarial' => 'required | string | max:50',
-            'facebook_marketplace' => 'required | string | max:50',
-            'pagina_web' => 'required | string | max:100',
-            'whatsapp_empresarial' => 'required | string | max:10',
-            'mercado_libre' => 'required | string | max:50',
-            'mercado_pago' => 'required | string | max:50',
+            //MEDIOS DIGITALES
+            'facebook_usuario' => ' | string | max:50',
+            'facebook_empresarial' => ' | string | max:50',
+            'facebook_marketplace' => ' | string | max:50',
+            'pagina_web' => ' | string | max:100',
+            'whatsapp_empresarial' => ' | string | max:10',
+            'mercado_libre' => ' | string | max:50',
+            'mercado_pago' => ' | string | max:50',
+            //tabla medios digitales adiconales
+            'medios_digitales_json' => '',
 
             //DOCUMENTOS
-            'ine_file' => 'required | file | mimes:pdf,jpg,jpeg,png',
-            'acta_nacimiento_file'=> 'required | file | mimes:pdf,jpg,jpeg,png',
-            'comprobante_domicilio_file' => 'required | file | mimes:pdf,jpg,jpeg,png',
-            'cif_file'=> 'required | file | mimes:pdf,jpg,jpeg,png',
-            'affy_file' => 'required | file | mimes:pdf,jpg,jpeg,png',
-            'impi_file' => 'required | file | mimes:pdf,jpg,jpeg,png',
-            'imss_file' => 'required | file | mimes:pdf,jpg,jpeg,png'
-            // -- falta la tabla de documentos adicionales --            
+            'ine_file' => ' | file | mimes:pdf,jpg,jpeg,png',
+            'acta_nacimiento_file'=> ' | file | mimes:pdf,jpg,jpeg,png',
+            'comprobante_domicilio_file' => ' | file | mimes:pdf,jpg,jpeg,png',
+            'cif_file'=> ' | file | mimes:pdf,jpg,jpeg,png',
+            'affy_file' => ' | file | mimes:pdf,jpg,jpeg,png',
+            'impi_file' => ' | file | mimes:pdf,jpg,jpeg,png',
+            'imss_file' => ' | file | mimes:pdf,jpg,jpeg,png',
+            // -- tabla de documentos adicionales --  
+            'documentos_adicionales_json' => ''          
         ]);
+        dd($validate);
 
         DB:: transaction(function () use ($validate) {
 
@@ -105,63 +112,70 @@ class Register extends Controller
                 
             ]);
 
-            $dato_fiscal = $persona-> dato_fiscal() -> create([
-                'str_razon_social' => $validate['razon_social'],
-                'str_nombre_comercial' => $validate['nombre_comercial'],
-                'int_tiempo_ejerciendo' => $validate['tiempo_ejerciendo'],
-                'str_rfc' => $validate['rfc_usuario'],
-                //'fk_regimen_id' => $validate['regimen_fiscal'],
-                'str_clave_impi' => $validate['registro_impi'],
-                'str_clvae_affy' => $validate['registro_affy'],
-                'str_clave_imss' => $validate['registro_imss'],
-                //'fk_actividad_economica_id' => $validate['actividad_economica'],
-                'int_num_empleados' => $validate['numero_empleados'],
-            ]);
+            // $dato_fiscal = $persona-> dato_fiscal() -> create([
+            //     'str_razon_social' => $validate['razon_social'],
+            //     'str_nombre_comercial' => $validate['nombre_comercial'],
+            //     'int_tiempo_ejerciendo' => $validate['tiempo_ejerciendo'],
+            //     'str_rfc' => $validate['rfc_usuario'],
+            //     //'fk_regimen_id' => $validate['regimen_fiscal'],
+            //     'str_clave_impi' => $validate['registro_impi'],
+            //     'str_clvae_affy' => $validate['registro_affy'],
+            //     'str_clave_imss' => $validate['registro_imss'],
+            //     //'fk_actividad_economica_id' => $validate['actividad_economica'],
+            //     'int_num_empleados' => $validate['numero_empleados'],
+            // ]);
 
-            foreach($validate['domicilios'] as $domicilio){
-                $domicilio = $dato_fiscal -> domicilio() -> create([
-                    'str_direccion' => $validate['direccion_domicilio'],
-                    'str_estado' => $validate['estado_domicilio'],
-                    'str_municipio' => $validate['municipio_domicilio'],
-                    'str_localidad' => $validate['localidad_domicilio'],
-                ]);
-            }
-            foreach($validate['registros_adicionales'] as $registro){
-                $registro = $dato_fiscal -> registro_adicional() -> create([
-                    'str_nombre_registro_adicional' => $validate['nombre_registro_adicional'],
-                    'str_clave_registro_adicional' => $validate['clave_registro_adicional'],
-                ]);
-            }
+            // //Añadir registros adiconales
+            // $registros_adicionales = json_decode($validate['registros_json'], true);
+            // foreach( $registros_adicionales as $registro ) {
+            //     $dato_fiscal ->adicional()->create([
+            //         'str_nombre_registro_adicional' => $registro[0],
+            //         'str_clave_registro_adicional'=> $registro[1],
+            //     ]);
+            // }
 
-            $productos_ventas = $dato_fiscal -> productos_ventas() -> create([
-                'str_nombre_producto' => $validate['nombre_producto'],
-                'str_descripcion_producto' => $validate['descripcion_producto'],
-                'int_produccion_mensual' => $validate['produccion_mensual'],
-                'int_venta_mensual' => $validate['venta_mensual'],
-                'int_venta_anual' => $validate['venta_anual']
-            ]);
+            // //Añador informacion de domicilio
+            // $domicilios = json_decode($validate['domicilios_json'], true);
+            // foreach($domicilios as $domicilio){
+            //     $domicilio_unitario = $dato_fiscal -> domicilio() -> create([
+            //         'str_direccion' => $domicilio[0],
+            //         'str_estado' => $domicilio[1],
+            //         'str_municipio' => $domicilio[2],
+            //         'str_localidad' => $domicilio[3]
+            //     ]);
+            // }
 
-            //Creacion de tabla redes sociales
-            $red_social = $dato_fiscal -> red_social() -> create([
-                'str_facebook' => $validate['facebook_usuario'],
-                'str_facebook_empresarial' => $validate['facebook_empresarial'],
-                'str_facebook_marketplace' => $validate['facebook_marketplace'],
-                'str_pagina_web' => $validate['pagina_web'],
-                'str_whatsapp_bussines' => $validate['whatsapp_empresarial'],
-                'str_mercado_libre' => $validate['mercado_libre'],
-                'str_mercado_pago' => $validate['mercado_pago'],
-            ]);
+            // $productos_ventas = json_decode($validate['productos_json'], true);
+            // foreach( $productos_ventas as $producto ) {
+            //     $producto_ventas = $dato_fiscal -> productos_ventas() -> create([
+            //         'str_nombre_producto' => $producto[0],
+            //         'str_descripcion_producto' => $producto[1],
+            //         'int_produccion_mensual' => $producto[2],
+            //         'int_venta_mensual' => $producto[3],
+            //         'int_venta_anual' => $producto[4]
+            //     ]);
+            // }
 
-            foreach($validate['redes_adicionales'] as $red_social){
-                $red_social = $dato_fiscal -> redes_adicionales() -> create([
-                    'str_nombre_red_social' => $validate['nombre_red_social'],
-                    'str_nombre_perfil' => $validate['nombre_perfil'],
-                ]);
-            }
+            // //Creacion de tabla redes sociales
+            // $red_social = $dato_fiscal -> red_social() -> create([
+            //     'str_facebook' => $validate['facebook_usuario'],
+            //     'str_facebook_empresarial' => $validate['facebook_empresarial'],
+            //     'str_facebook_marketplace' => $validate['facebook_marketplace'],
+            //     'str_pagina_web' => $validate['pagina_web'],
+            //     'str_whatsapp_bussines' => $validate['whatsapp_empresarial'],
+            //     'str_mercado_libre' => $validate['mercado_libre'],
+            //     'str_mercado_pago' => $validate['mercado_pago'],
+            // ]);
 
+            // $red_adicional = json_decode($validate['medios_digitales_json'], true);
+            // foreach( $red_adicional as $red_social){
+            //     $red_social_adicional = $dato_fiscal -> redes_adicionales() -> create([
+            //         'str_nombre_red_social' => $red_social[0],
+            //         'str_nombre_perfil' => $red_social[1]
+            //     ]);
+            // }
         });
-
-        
+        return redirect('/');
     }
 
     /**
--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Persona extends Model
 {
     //
+    protected $table = [
+        'str_curp',
+        'str_nombre',
+        'str_apellido_paterno',
+        'str_apellido_materno',
+        'dt_fecha_nacimiento',
+        'str_estado_nacimiento',
+        'str_municipio_nacimiento' ,
+        'str_sexo',
+        'bl_mayahablante',
+        'str_ine',
+        'str_correo_electronico',
+        'str_tel_celular',
+        'estado_perfil',
+    ];
 }
