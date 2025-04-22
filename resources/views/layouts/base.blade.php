ec5dac250fc79e7efdb2a45f2e1e6dd8b8aa0f12
GabrielManzanilla <gabriel29444@gmail.com>
2025-04-22 09:15:03 -0600

    Feat(resources/views/layouts/base.blade.php) Add Dropdown with menu for consultes
    This is only view, the options not redirect for any (even)

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -49,21 +49,51 @@
 					</li>
 					<li>
 						<a href="/mujer-transformadora/public/about/"
-							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-white md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
 					</li>
 					<li>
 						<a href="{{ route('register.create') }}"
-							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Registro</a>
-					</li>
-					<li>
-						<!-- Este sera un dropdown para desplegar las opciones de vista-->
-						<a href="#"
-							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Consulta</a>
+							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-white md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Registro</a>
 					</li>
 					<li>
 						<a href="/contactos/"
-							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contactos</a>
+							class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-white md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contactos</a>
 					</li>
+					<li>
+						<button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+							class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-gray-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Consultas
+							<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+								viewBox="0 0 10 6">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+									d="m1 1 4 4 4-4" />
+							</svg></button>
+						<!-- Dropdown menu -->
+						<div id="dropdownNavbar"
+							class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-[#6D1528] dark:divide-gray-200">
+							<ul class="py-2 text-sm text-white dark:text-white" aria-labelledby="dropdownLargeButton">
+								<li>
+									<a href="#"
+										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Usuarios</a>
+								</li>
+								<li>
+									<a href="#"
+										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Buscar Usuario</a>
+								</li>
+							</ul>
+							<div class="py-1">
+							<ul class="py-2 text-sm text-white dark:text-white" aria-labelledby="dropdownLargeButton">
+								<li>
+									<a href="#"
+										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Registros</a>
+								</li>
+								<li>
+									<a href="#"
+										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Buscar Registro</a>
+								</li>	
+							</div>
+						</div>
+					</li>
+
 				</ul>
 			</div>
 		</div>
